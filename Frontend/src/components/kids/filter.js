837cf710542ff0277487a0b7fa1f1a1e2ca105be
varthadinesh <30522988+varthadinesh@users.noreply.github.com>
2024-03-04 20:33:05 +0530

    Add files via upload

---
+++ Frontend/src/components/kids/filter.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+
+const Filter = ({ products, onFilter }) => {
+  const [selectedSize, setSelectedSize] = useState("");
+
+  // Retrieve the selected size from local storage when the component mounts
+  useEffect(() => {
+    const storedSize = localStorage.getItem("selectedSize");
+    if (storedSize) {
+      setSelectedSize(storedSize);
+    }
+  }, []);
+
+  const handleSizeSelect = (size) => {
+    setSelectedSize(size);
+    const filtered = products.filter((product) => product.size === size);
+    onFilter(filtered);
+    // Store the selected size in local storage
+    localStorage.setItem("selectedSize", size);
+  };
+
+  const handleClearFilter = () => {
+    setSelectedSize(""); // Clear the selected size
+    localStorage.removeItem("selectedSize"); // Remove the selected size from local storage
+    onFilter(products); // Reset filtering
+  };
+
+  return (
+    <>
+      <div className="card mt-4 mb-5">
+        <div className="card-header bg-secondary text-white p-3 fs-5">
+          <i className="bi bi-sort-down-alt pe-3"></i>
+          Filter by attributes
+        </div>
+        <div className="card-body">
+          <h5>Size</h5>
+          <div className="d-flex">
+            <button
+              className={`btn btn-outline-secondary m-2 ${
+                selectedSize === "XS" ? "active" : ""
+              }`}
+              onClick={() => handleSizeSelect("XS")}
+            >
+              XS
+            </button>
+            <button
+              className={`btn btn-outline-secondary m-2 ${
+                selectedSize === "S" ? "active" : ""
+              }`}
+              onClick={() => handleSizeSelect("S")}
+            >
+              S
+            </button>
+            <button
+              className={`btn btn-outline-secondary m-2 ${
+                selectedSize === "M" ? "active" : ""
+              }`}
+              onClick={() => handleSizeSelect("M")}
+            >
+              M
+            </button>
+            <button
+              className={`btn btn-outline-secondary m-2 ${
+                selectedSize === "L" ? "active" : ""
+              }`}
+              onClick={() => handleSizeSelect("L")}
+            >
+              L
+            </button>
+            <button
+              className={`btn btn-outline-secondary m-2 ${
+                selectedSize === "XL" ? "active" : ""
+              }`}
+              onClick={() => handleSizeSelect("XL")}
+            >
+              XL
+            </button>
+          </div>
+          {selectedSize && (
+            <button
+              className="btn btn-outline-secondary"
+              onClick={handleClearFilter}
+            >
+              Remove Filter
+            </button>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Filter;
---
+++ Frontend/src/components/register.js
@@ -0,0 +1,183 @@
+import React, { useState } from "react";
+import MyNavbar from "./navbar";
+import { useNavigate } from "react-router-dom";
+import Footer from "./footer";
+
+const Register = () => {
+  const [values, setValues] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    phone: "",
+    password: "",
+    confirmpassword: "",
+  });
+  const navigate = useNavigate();
+
+  const handleInput = (event) => {
+    setValues((prev) => ({
+      ...prev,
+      [event.target.name]: [event.target.value],
+    }));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (values.password[0] === values.confirmpassword[0]) {
+      navigate("/emailverification", { state: { values } });
+    } else {
+      alert("Passwords do not match");
+    }
+  };
+
+  return (
+    <div className="fullscreen">
+      <MyNavbar />
+      <main>
+      <div className="p-2 ps-lg-5 pe-lg-5 mb-5">
+        <div className="col-xs-12 col-md-12 col-lg-12">
+          <form method="post" onSubmit={handleSubmit}>
+            <div>
+              <div>
+                <h3 className="text-center">Create Account</h3>
+              </div>
+              <hr />
+              <div className="form-group d-md-flex justify-content-center">
+                <label
+                  className="control-label col-sm-2 col-md-2"
+                  htmlFor="firstname"
+                >
+                  First Name
+                </label>
+                <div className="col-sm-6 col-md-4 col-xs-12">
+                  <input
+                    className="form-control mb-2"
+                    type="text"
+                    id="firstname"
+                    name="firstname"
+                    onChange={handleInput}
+                    pattern="[A-Z][a-z]*\s*\w*"
+                    title="First letter should be uppercase, remaining letters are lowercase. No special characters"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="form-group  d-md-flex justify-content-center">
+                <label
+                  className="control-label col-sm-2 col-md-2"
+                  htmlFor="lastname"
+                >
+                  Last Name
+                </label>
+                <div className="col-sm-6 col-md-4 col-xs-12">
+                  <input
+                    className="form-control mb-2"
+                    type="text"
+                    id="lastname"
+                    name="lastname"
+                    onChange={handleInput}
+                    pattern="[A-Z][a-z]*\s*\w*"
+                    title="First letter should be uppercase, remaining letters are lowercase. No special characters"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="form-group  d-md-flex justify-content-center">
+                <label
+                  className="control-label col-sm-2 col-md-2"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <div className="col-sm-6 col-md-4 col-xs-12">
+                  <input
+                    className="form-control mb-2"
+                    type="email"
+                    id="email"
+                    name="email"
+                    onChange={handleInput}
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="form-group  d-md-flex justify-content-center">
+                <label
+                  className="control-label col-sm-2 col-md-2"
+                  htmlFor="phone"
+                >
+                  Phone
+                </label>
+                <div className="col-sm-6 col-md-4 col-xs-12">
+                  <input
+                    className="form-control mb-2"
+                    type="tel"
+                    id="phone"
+                    name="phone"
+                    onChange={handleInput}
+                    pattern="[0-9]{10}"
+                    title="10 digit numeric value only"
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="form-group  d-md-flex justify-content-center">
+                <label
+                  className="control-label col-sm-2 col-md-2"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <div className="col-sm-6 col-md-4 col-xs-12">
+                  <input
+                    className="form-control mb-2"
+                    type="password"
+                    id="password"
+                    name="password"
+                    onChange={handleInput}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="form-group  d-md-flex justify-content-center">
+                <label
+                  className="control-label col-sm-2 col-md-2"
+                  htmlFor="confirmpassword"
+                >
+                  Confirm Password
+                </label>
+                <div className="col-sm-6 col-md-4 col-xs-12">
+                  <input
+                    className="form-control mb-2"
+                    type="password"
+                    id="confirmpassword"
+                    name="confirmpassword"
+                    onChange={handleInput}
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="form-group  d-md-flex justify-content-center">
+              <div className="col-sm-2 col-md-2"></div>
+              <div className="col-sm-6 col-md-4 col-xs-12 text-center">
+                <button
+                  type="submit"
+                  className="btn btn-primary register-next-step-button w-50"
+                >
+                  Register
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      </main>
+      <Footer/>
+    </div>
+  );
+};
+
+export default Register;
